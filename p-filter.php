6f33cfbca321cb521a42035cadb393bf138b197a
= <irmarista16@gmail.com>
2022-05-31 21:18:48 +0700

    login dan logout

--- p-filter.php
+++ p-filter.php
@@ -4,36 +4,40 @@ $ceka = $_POST['ya'];
 
 $jumlaha = count($ceka) * 1;
 
-$tampil = "SELECT * FROM warga ORDER BY id_warga desc LIMIT 1";
+$tampil = "SELECT * FROM warga ORDER BY nik desc LIMIT 1";
 $hasil = mysqli_query($koneksi, $tampil);
 
 while ($data = mysqli_fetch_array($hasil)) {
-    $id_warga = $data['id_warga'];
+    $id_warga = $data['nik'];
 }
 //echo $id_warga;
 
 for ($i = 0; $i < $jumlaha; $i++) {
-    if (mysqli_query($koneksi, "INSERT INTO kriteria_warga (id_kriteria, id_warga) values ($ceka[$i], $id_warga)")) {
+    if (mysqli_query($koneksi, "INSERT INTO jawaban_survey (id_kriteria, id_survey) values ($ceka[$i], $nik)")) {
     } else {
         echo mysqli_error($koneksi);
     }
 }
-$result = mysqli_query($koneksi, "SELECT COUNT(*) as total FROM kriteria_warga WHERE id_warga = $id_warga");
+$result = mysqli_query($koneksi, "SELECT COUNT(*) as total FROM jawaban_survey WHERE nik = $nik");
 $row = mysqli_fetch_array($result);
 $count = $row['total'];
 echo $count;
 
+$sql = mysqli_query($koneksi, "SELECT FROM status WHERE nama_status = min");
+$min = mysqli_fetch_array($sql);
+$sql2 = mysqli_query($koneksi, "SELECT FROM status WHERE nama_status = max");
+$max = mysqli_fetch_array($sql2);
 $status = "";
-if ($count >= 11) {
+if ($count >= $max) {
     $status = 'Sangat Layak';
-} else if ($count <= 10 && $count >= 6) {
+} else if ($count <= $max && $count >= $min) {
     $status = 'Layak';
 } else {
     $status = 'Tidak Layak';
 }
 
 echo $status;
-if (mysqli_query($koneksi, "UPDATE warga SET status='$status' WHERE id_warga='$id_warga'")) {
+if (mysqli_query($koneksi, "UPDATE survey SET status='$status' WHERE nik='$nik'")) {
     header("location:a-tambah_data.php");
 }
 //echo $ceka[0];
